60ca08c4ed9f4f1e7df94c597cf28c10bc94e3c6
“wenshan” <893192435@qq.com>
2021-01-09 15:14:42 +0800

    :art: 用户个人信息

--- app/api/user.js
+++ app/api/user.js
@@ -12,4 +12,10 @@ router.get('/', async (ctx) => {
 router.get('/map', async (ctx) => {
     ctx.body = ctx.renderJson({ msg: '查询成功', data: await userMap(ctx.header.token) });
 });
+/**
+ * 用户个人信息
+ */
+router.get('/myUser', async (ctx) => {
+    ctx.body = { code: 1000, data: ctx.state };
+});
 export default router;
